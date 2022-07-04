3a745b8b5ec2a307f92ab48ecf44e0a528c5a72b
philyb1701 <philip.burkhill@gmail.com>
2022-07-04 22:50:27 +0100

    added city. need to add other fields next.

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/addproperty.css";
 
 const AddProperty = () => {
-  return <div>Add Property Page</div>;
+  const initialState = {
+    fields: {
+      title: "",
+      city: "Manchester",
+    },
+  };
+
+  const [fields, setFields] = useState(initialState.fields);
+
+  const handleAddProperty = (event) => {
+    event.PreventDefault();
+  };
+
+  const handleFieldChange = (event) => {
+    setFields({ ...fields, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="add-property">
+      <h1>Add Property Page</h1>
+      <form onSubmit={handleAddProperty}>
+        <label htmlFor="title">
+          Title
+          <input
+            id="title"
+            name="title"
+            value={fields.title}
+            onChange={handleFieldChange}
+          />
+        </label>
+
+        <label htmlFor="city">City</label>
+        <select
+          id="city"
+          name="city"
+          value={fields.city}
+          onChange={handleFieldChange}
+        >
+          <option value="Manchester">Manchester</option>
+          <option value="Leeds">Leeds</option>
+          <option value="Sheffield">Sheffield</option>
+          <option value="Liverpool">Liverpool</option>
+        </select>
+
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddProperty;
