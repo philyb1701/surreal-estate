a695ac420e687baacb5b51d3b1872fa968f84391
philyb1701 <philip.burkhill@gmail.com>
2022-07-06 18:31:00 +0100

    Form completed and functioning

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -6,6 +6,11 @@ const AddProperty = () => {
     fields: {
       title: "",
       city: "Manchester",
+      type: "Flat",
+      bedrooms: "",
+      bathrooms: "",
+      price: "",
+      email: "",
     },
   };
 
@@ -29,6 +34,7 @@ const AddProperty = () => {
             id="title"
             name="title"
             value={fields.title}
+            placeholder="Property name"
             onChange={handleFieldChange}
           />
         </label>
@@ -46,6 +52,70 @@ const AddProperty = () => {
             <option value="Sheffield">Sheffield</option>
             <option value="Liverpool">Liverpool</option>
           </select>
+        </label>
+
+        <label htmlFor="type">
+          Property Type:
+          <select
+            id="type"
+            name="type"
+            value={fields.type}
+            onChange={handleFieldChange}
+          >
+            <option value="Flat">Flat</option>
+            <option value="Detached">Detached</option>
+            <option value="Semi-Detached">Semi-Detached</option>
+            <option value="Terraced">Terraced</option>
+            <option value="End of Terrace">End of Terrace</option>
+            <option value="Cottage">Cottage</option>
+            <option value="Bungalow">Bungalow</option>
+          </select>
+        </label>
+
+        <label htmlFor="bedroom">
+          Bedrooms:
+          <input
+            type="number"
+            id="bedrooms"
+            name="bedrooms"
+            value={fields.bedrooms}
+            placeholder="1"
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="bathroom">
+          Bathrooms:
+          <input
+            type="number"
+            id="bathrooms"
+            name="bathrooms"
+            value={fields.bathrooms}
+            placeholder="1"
+            onChange={handleFieldChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price (£):
+          <input
+            type="number"
+            id="price"
+            name="price"
+            value={fields.price}
+            placeholder="50,000"
+            onChange={handleFieldChange}
+          />
+        </label>
+
+        <label htmlFor="email">
+          email:
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={fields.email}
+            placeholder="Enter email..."
+            onChange={handleFieldChange}
+          />
         </label>
         <button type="submit">Add</button>
       </form>
