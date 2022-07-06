84c31e78dbda3e0f356afd008adb44c07ea898c5
philyb1701 <philip.burkhill@gmail.com>
2022-07-06 16:51:11 +0100

    fixed eslint error

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -33,19 +33,20 @@ const AddProperty = () => {
           />
         </label>
 
-        <label htmlFor="city">City</label>
-        <select
-          id="city"
-          name="city"
-          value={fields.city}
-          onChange={handleFieldChange}
-        >
-          <option value="Manchester">Manchester</option>
-          <option value="Leeds">Leeds</option>
-          <option value="Sheffield">Sheffield</option>
-          <option value="Liverpool">Liverpool</option>
-        </select>
-
+        <label htmlFor="city">
+          City:
+          <select
+            id="city"
+            name="city"
+            value={fields.city}
+            onChange={handleFieldChange}
+          >
+            <option value="Manchester">Manchester</option>
+            <option value="Leeds">Leeds</option>
+            <option value="Sheffield">Sheffield</option>
+            <option value="Liverpool">Liverpool</option>
+          </select>
+        </label>
         <button type="submit">Add</button>
       </form>
     </div>
