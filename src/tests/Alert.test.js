26a6a854319c3990b555957239717fa1c8e90ca1
philyb1701 <philip.burkhill@gmail.com>
2022-07-21 22:30:46 +0100

    tests added

--- src/tests/Alert.test.js
+++ src/tests/Alert.test.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Alert from "../components/App";
 
 test("displays an error message", () => {
-  const { getByText } = render(<Alert message="Error!" />);
+  const { getByText, asFragment } = render(<Alert message="Error!" />);
 
   expect(getByText(/Error/).textContent).toBe("Error!");
+  expect(asFragment()).toMatchSnapshot();
 });
 
 test("displays a success message", () => {
   const { getByText } = render(<Alert message="Success!!!!" success />);
 
   expect(getByText(/Success/).textContent).toBe("Success!!!!");
+});
+
+test("does not render an error or a success message if message props is empty", () => {
+  const { asFragment } = render(<Alert message="" />);
+  const alert = asFragment();
+
+  expect(alert).toMatchSnapshot();
 });
